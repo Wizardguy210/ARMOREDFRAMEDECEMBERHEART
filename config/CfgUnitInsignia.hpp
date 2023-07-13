17b7a7964518563a70892ae3641dada0f41c01c5
Wizardguy210 <136212454+Wizardguy210@users.noreply.github.com>
2023-07-13 11:34:58 -0500

    Wizardguy210/SonixApache

--- config/CfgUnitInsignia.hpp
+++ config/CfgUnitInsignia.hpp
@@ -1,12 +1,12 @@
-class snx_ins_SelectionUniversity
+#  class snx_ins_SelectionUniversity
 	{
-		displayName = "Selection University Tank Team"; 
-		author = "SonixApache"; 
-		texture = "images\SelectionUniversityPatch.paa";
+#  		displayName = "Selection University Tank Team"; 
+#  		author = "SonixApache"; 
+#  		texture = "images\SelectionUniversityPatch.paa";
 	};
-class snx_ins_SupportSquad
+#  class snx_ins_SupportSquad
 	{
-		displayName = "Support Squad"; 
-		author = "SonixApache"; 
-		texture = "images\SupportFlat.paa";
+#  		displayName = "Support Squad"; 
+#  		author = "SonixApache"; 
+#  		texture = "images\SupportFlat.paa";
 	};
